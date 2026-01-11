8f651641cda9435b83c26ca8982f29ec9e110fef
sid-darth-73 <siddharthraj532@gmail.com>
2026-01-11 14:45:41 +0530

    dev: fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/attendance-tracker">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/status" element={<Status />} />
